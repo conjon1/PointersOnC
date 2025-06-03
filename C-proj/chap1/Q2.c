fe72c8797c2500bd17b6cc1dee40e254222e43ac
conjon1 <170683676+conjon1@users.noreply.github.com>
2025-06-03 00:51:57 -0700

    notes and problems in the book

--- C-proj/chap1/Q2.c
+++ C-proj/chap1/Q2.c
@@ -1,9 +1,42 @@
 #include <stdio.h>
 
-int main(void) {
-  char hi[] = "Hello World!";
+// write a program that reads from the standard intput.
+// each line is printed on the standard output preceded by its line number.
+// try to write the program so that it has no built in limit on how long a line it can handle 
 
-  printf("%s\n", hi);
+
+int main(void) {
+  int ch;
+  int lineNumber;
+  int atBeg;
+
+  // need to write a loop to make sure that getchar does not return eof 
+  while ((ch = getchar()) != EOF){
+    if (atBeg) {
+      printf("%d: ", lineNumber);
+      atBeg = 0;
+
+    }
+    putchar(ch);
+
+    // need it increment the line number
+    if (ch == '\n') {
+      lineNumber++;
+      atBeg = 1;
+
+
+    }
+
+
+  } 
+
+
 
   return 0;
+
 }
+
+
+
+
+
