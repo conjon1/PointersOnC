5ee801a51f36105648b18ec70cc2c29fab520970
conjon1 <170683676+conjon1@users.noreply.github.com>
2025-06-03 18:16:41 -0700

    notes and problems in the book

--- C-proj/chap1/Q3.c
+++ C-proj/chap1/Q3.c
@@ -1,3 +1,23 @@
+#include <stdio.h>
 
 
+
+// write program that reads characters from the input and writes them to output.
+// it should compute the the checksum and write it out after the characters .
+// The checksum is computed in a signed char varible that is initiallized to -1.
+// as each character is read from the input it id added to the checksum .
+// any overflow from the checksum should be ignored
+// when all of the chararcters have been written the checksum is then written as a decimal interger which may be negitive.
+// be sure to follow the checksum with a new line
+// on ASCII it should be
+// Hello world!
+// 102
+
+
+int ch; 
+main(void)
+{  
+  while((ch = getchar()) != EOF && ch = '\n' ); 
+
+};
 
